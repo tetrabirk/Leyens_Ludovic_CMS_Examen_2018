fb2296099e45d9482af16927d3c4a5aa944f1b1c
tetrabirk <ludovic.leyens@gmail.com>
2018-01-27 20:43:39 +0100

    single - titre

--- include/single/titre.php
+++ include/single/titre.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Ludovic
- * Date: 27/01/2018
- * Time: 19:45
- */
+<section id="subheader" data-speed="8" data-type="background" class="padding-top-bottom subheader" style="background:url(<?php echo wp_get_attachment_image_src(get_post_thumbnail_id(), 'full', true)[0]; ?>) top fixed;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1><?php the_title()?></h1>
+                <ul class="breadcrumbs">
+                    <li><a href="<?php echo get_home_url()?>"><?php _e('Home', 'wp-theme-base-translate'); ?></a></li>
+                    <b>/</b>
+                    <li class="active"><?php the_title()?></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
