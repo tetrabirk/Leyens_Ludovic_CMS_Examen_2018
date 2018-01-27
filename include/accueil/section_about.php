2dc779edca0898711a654f8fbee0d00c926f498d
tetrabirk <ludovic.leyens@gmail.com>
2018-01-27 17:04:37 +0100

    front - section 1 - correction

--- include/accueil/section_about.php
+++ include/accueil/section_about.php
@@ -3,13 +3,13 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-sm-4">
-                <img src="<?php the_field('accueil_1er_section_image',121); ?>" class="img-responsive"/>
+                <img src="<?php the_field('accueil_1er_section_image'); ?>" class="img-responsive"/>
             </div>
             <div class="col-md-8 col-sm-8">
                 <div class="about-box">
-                    <h2 class="box-title"><?php the_field('accueil_1er_section_texte',121); ?></h2>
+                    <h2 class="box-title"><?php the_field('accueil_1er_section_texte'); ?></h2>
                     <div class="tiny-border"></div>
-                    <p><?php the_field('accueil_1er_section_wysiwyg',121); ?></p>
+                    <p><?php the_field('accueil_1er_section_wysiwyg'); ?></p>
                 </div>
             </div>
         </div>
