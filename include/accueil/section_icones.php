6e53885e818ee32a3a08a947f958266103a90779
tetrabirk <ludovic.leyens@gmail.com>
2018-01-27 17:05:17 +0100

    front - section 3

--- include/accueil/section_icones.php
+++ include/accueil/section_icones.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Ludovic
- * Date: 27/01/2018
- * Time: 16:06
- */
+<!-- section begin -->
+<section>
+    <!-- Container Begin -->
+    <div class="container">
+        <div class="row">
+            <?php while (have_rows('accueil_3eme_section')) : the_row(); ?>
+                <div class="col-md-4">
+                    <div class="service-box service-style2">
+
+                        <img src="<?php the_sub_field('accueil_3eme_section_icon'); ?>"
+                             class="img-responsive"/>
+                        <div class="service-content">
+                            <h3><?php the_sub_field('accueil_3eme_section_titre'); ?></h3>
+                            <p><?php the_sub_field('accueil_3eme_section_wysiwyg'); ?></p>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
+    <!-- Container End -->
+</section>
+<!-- section close -->
