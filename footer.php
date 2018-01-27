0659a4ea535036a02cd0b5091232eff50717c961
tetrabirk <ludovic.leyens@gmail.com>
2018-01-27 15:40:48 +0100

    creation footer et header + debut front-page

--- footer.php
+++ footer.php
@@ -1,7 +1,28 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Ludovic
- * Date: 27/01/2018
- * Time: 15:11
- */
+<a id="to-the-top" ><i class="fa fa-angle-up"></i></a>
+
+<!-- LOAD JS FILES -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/imagesloaded.pkgd.min.js"></script>
+<script src="js/easing.js"></script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/jquery.fitvids.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+
+<!-- Waypoints-->
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/sticky.min.js"></script>
+<script src="js/tipper.js"></script>
+<script src="js/compact.js"></script>
+<script src="js/custom-index1.js"></script>
+
+<!-- SLIDER REVOLUTION SCRIPTS  -->
+<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script src="js/revslider-custom.js"></script>
+
+<?php wp_footer();?>
+
+</body>
+</html>
