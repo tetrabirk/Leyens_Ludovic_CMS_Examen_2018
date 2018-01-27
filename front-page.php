0103cb0c217a4fa934136fcd57a50c864e14e466
tetrabirk <ludovic.leyens@gmail.com>
2018-01-27 17:06:30 +0100

    front

--- front-page.php
+++ front-page.php
@@ -1,272 +1,29 @@
-<?php get_header(); ?>
-    <!-- slider -->
-    <div id="slider">
-        <!-- revolution slider begin -->
-        <div class="fullwidthbanner-container">
-            <div id="revolution-slider">
-                <ul>
-                    <li data-transition="fade" data-slotamount="7" data-masterspeed="2500" data-delay="5000">
-                        <!--  BACKGROUND IMAGE -->
-                        <img src="img-rev-slider/bg-1.jpg" alt="">
+<?php
+/**
+ * Template Name: HomePage
+ *
+ */
+get_header();
+?>
 
-                    </li>
 
-                    <li data-transition="fade" data-slotamount="7" data-masterspeed="2500" data-delay="5000">
-                        <!--  BACKGROUND IMAGE -->
-                        <img src="img-rev-slider/bg-3.jpg" alt="">
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
 
-                    </li>
+        <?php include_once 'include/accueil/slider.php' ?>
 
-                    <li data-transition="fade" data-slotamount="7" data-masterspeed="2500" data-delay="5000">
-                        <!--  BACKGROUND IMAGE -->
-                        <img src="img-rev-slider/bg-2.jpg" alt="">
-                    </li>
-                </ul>
-            </div>
+        <div class="clearfix"></div>
+
+        <!-- content begin -->
+        <div id="content" class="no-padding">
+            <?php include_once 'include/accueil/section_about.php' ?>
+
+            <?php include_once 'include/accueil/section_derniers_articles.php' ?>
+
+            <?php include_once 'include/accueil/section_icones.php' ?>
         </div>
-        <!-- revolution slider close -->
-    </div>
-    <!-- slider close -->
+        <!-- content close -->
+    <?php endwhile; ?>
+<?php endif; ?>
 
-    <div class="clearfix"></div>
-
-    <!-- content begin -->
-    <div id="content" class="no-padding">
-
-        <!-- section begin -->
-        <section id="section-about">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-sm-4">
-                        <img src="images/about/we-are-thumb.jpg" alt="about image" class="img-responsive">
-                    </div>
-                    <div class="col-md-8 col-sm-8">
-                        <div class="about-box">
-                            <h2 class="box-title">We are Compact</h2>
-                            <div class="tiny-border"></div>
-                            <p>Morbi vehicula a nibh in commodo. Aliquam quis dolor eget lectus pulvinar malesuada. Suspendisse eu rhoncus ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra,</p>
-                            <p>Morbi vehicula a nibh in commodo. Aliquam quis dolor eget lectus pulvinar malesuada. Suspendisse eu rhoncus ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra,</p>
-                            <p>Morbi vehicula a nibh in commodo. Aliquam quis dolor eget lectus pulvinar malesuada. Suspendisse eu rhoncus ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra,</p>
-                            <p>Morbi vehicula a nibh in commodo. Aliquam quis dolor eget lectus pulvinar malesuada. Suspendisse eu rhoncus ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra,</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- section close -->
-
-        <!-- section begin -->
-        <section id="section-project" class="no-padding">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="latest-projects clearfix">
-                            <div class="latest-projects-intro">
-                                <h2 class="box-title">Derniers articles</h2>
-                                <div class="tiny-border-light"></div>
-                                <p>Pellentesque gravida diam orci, vitae venenatis est eleifend sed. Proin non pretium turpis</p>
-                            </div>
-                            <div class="latest-projects-wrapper">
-                                <div id="latest-projects-items" class="latest-projects-items">
-                                    <div class="item">
-                                        <img src="images/projects/small-2.jpg" alt="" class="img-responsive">
-                                        <div class="project-details">
-                                            <p class="folio-title"><a href="#">Plan For Your Bussiness</a></p>
-                                            <p class="folio-cate"><i class="fa fa-tag"></i><a href="#">Finance</a>, <a href="#">Marketing</a></p>
-                                            <div class="folio-buttons">
-                                                <a href="images/projects/medium-1.jpg" title="Plan For Your Bussiness" class="folio"><i class="fa fa-search"></i></a>
-                                                <a href="#"><i class="fa fa-link"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item">
-                                        <img src="images/projects/small-3.jpg" alt="" class="img-responsive">
-                                        <div class="project-details">
-                                            <p class="folio-title"><a href="#">Business Growth Solutions</a></p>
-                                            <p class="folio-cate"><i class="fa fa-tag"></i><a href="#">Finance</a>, <a href="#">Marketing</a></p>
-                                            <div class="folio-buttons">
-                                                <a href="images/projects/medium-2.jpg" title="Business Growth Solutions" class="folio"><i class="fa fa-search"></i></a>
-                                                <a href="#"><i class="fa fa-link"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item">
-                                        <img src="images/projects/small-4.jpg" alt="" class="img-responsive">
-                                        <div class="project-details">
-                                            <p class="folio-title"><a href="#">Enterprise Development</a></p>
-                                            <p class="folio-cate"><i class="fa fa-tag"></i><a href="#">Finance</a>, <a href="#">Marketing</a></p>
-                                            <div class="folio-buttons">
-                                                <a href="images/projects/medium-3.jpg" title="Enterprise Development" class="folio"><i class="fa fa-search"></i></a>
-                                                <a href="#"><i class="fa fa-link"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item">
-                                        <img src="images/projects/small-1.jpg" alt="" class="img-responsive">
-                                        <div class="project-details">
-                                            <p class="folio-title"><a href="#">Successful Business</a></p>
-                                            <p class="folio-cate"><i class="fa fa-tag"></i><a href="#">Finance</a>, <a href="#">Marketing</a></p>
-                                            <div class="folio-buttons">
-                                                <a href="images/projects/medium-4.jpg" title="Successful Business" class="folio"><i class="fa fa-search"></i></a>
-                                                <a href="#"><i class="fa fa-link"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item">
-                                        <img src="images/projects/small-5.jpg" alt="" class="img-responsive">
-                                        <div class="project-details">
-                                            <p class="folio-title"><a href="#">Marketing Strategy</a></p>
-                                            <p class="folio-cate"><i class="fa fa-tag"></i><a href="#">Finance</a>, <a href="#">Marketing</a></p>
-                                            <div class="folio-buttons">
-                                                <a href="images/projects/medium-5.jpg" title="Marketing Strategy" class="folio"><i class="fa fa-search"></i></a>
-                                                <a href="#"><i class="fa fa-link"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item">
-                                        <img src="images/projects/small-6.jpg" alt="" class="img-responsive">
-                                        <div class="project-details">
-                                            <p class="folio-title"><a href="#">Effective Recruitment</a></p>
-                                            <p class="folio-cate"><i class="fa fa-tag"></i><a href="#">Finance</a>, <a href="#">Marketing</a></p>
-                                            <div class="folio-buttons">
-                                                <a href="images/projects/medium-6.jpg" title="Effective Recruitment" class="folio"><i class="fa fa-search"></i></a>
-                                                <a href="#"><i class="fa fa-link"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- section close -->
-
-        <!-- section begin -->
-        <section>
-            <!-- Container Begin -->
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="service-box service-style2">
-                            <img src="images/services/thumbs-1.png" alt="" class="img-responsive">
-                            <div class="service-content">
-                                <h3>Mobile Ready</h3>
-                                <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="service-box service-style2">
-                            <img src="images/services/thumbs-2.png" alt="" class="img-responsive">
-                            <div class="service-content">
-                                <h3>Very Easy Customizie</h3>
-                                <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="service-box service-style2">
-                            <img src="images/services/thumbs-3.png" alt="" class="img-responsive">
-                            <div class="service-content">
-                                <h3>Free Icon Font Awesome</h3>
-                                <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Container End -->
-        </section>
-        <!-- section close -->
-
-    </div>
-    <!-- content close -->
-
-    <!-- footer begin -->
-    <footer class="footer-1 bg-color-1">
-
-        <!-- main footer begin -->
-        <div class="main-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3 col-sm-6">
-                        <div class="compact-widget">
-                            <div class="widget-inner">
-                                <h3 class="widget-title">Résumé</h3>
-                                <p>Compact is a clean PSD theme suitable for corporate, You can customize it very easy to fit your needs, semper suscipit metus accumsan at. Vestibulum et lacus urna. Nam luctus ac tortor eu</p>
-                                <div class="social-icons clearfix">
-                                    <a href="#" class="facebook tipped" data-title="facebook"  data-tipper-options='{"direction":"top","follow":"true"}'><i class="fa fa-facebook"></i></a>
-                                    <a href="#" class="twitter tipped" data-title="twitter"  data-tipper-options='{"direction":"top","follow":"true"}'><i class="fa fa-twitter"></i></a>
-                                    <a href="#" class="google tipped" data-title="google +"  data-tipper-options='{"direction":"top","follow":"true"}'><i class="fa fa-google-plus"></i></a>
-                                    <a href="#" class="youtube tipped" data-title="youtube"  data-tipper-options='{"direction":"top","follow":"true"}'><i class="fa fa-youtube-play"></i></a>
-                                    <a href="#" class="linkedin tipped" data-title="linkedin"  data-tipper-options='{"direction":"top","follow":"true"}'><i class="fa fa-linkedin"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-3 col-sm-6">
-                        <div class="compact-widget">
-                            <h3 class="widget-title">Catégories</h3>
-                            <div class="widget-inner">
-                                <ul>
-                                    <li><a href="categories.html">Finance</a></li>
-                                    <li><a href="categories.html">Bilan</a></li>
-                                    <li><a href="categories.html">Conseils</a></li>
-                                    <li><a href="categories.html">Juridique</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-3 col-sm-6">
-                        <div class="compact-widget">
-                            <h3 class="widget-title">Contact</h3>
-                            <div class="widget-inner">
-                                <p>Address: 379 5th Ave  New York, NYC <br> 10018, United States</p>
-                                <p>Phone: +(112) 345 6879</p>
-                                <p>Fax: +(112) 345 8796</p>
-                                <p>Email: contact@compact.com</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-3 col-sm-6">
-                        <div class="compact-widget">
-                            <h3 class="widget-title">Newsletter</h3>
-                            <div class="widget-inner">
-                                <div class="newsletter newsletter-widget">
-                                    <p>Stay informed about our news and events</p>
-                                    <form action="" method="post">
-                                        <p><input class="newsletter-email" type="email" name="email" placeholder="Your email"><i class="fa fa-envelope-o"></i></p>
-                                        <p><input class="newsletter-submit" type="submit" value="Subscribe"></p>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        <!-- main footer close -->
-
-        <!-- sub footer begin -->
-        <div class="sub-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        Copyright &copy; 2016 Designed by AuThemes. All rights reserved.
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- sub footer close -->
-
-    </footer>
-    <!-- footer close -->
-
-</div>
 <?php get_footer(); ?>
