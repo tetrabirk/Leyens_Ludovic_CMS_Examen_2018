e694a16f881339f1dd5f04c6ae62afaa8f2d5578
tetrabirk <ludovic.leyens@gmail.com>
2018-01-27 17:16:18 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,87 @@
+<!-- footer begin -->
+<footer class="footer-1 bg-color-1">
+
+    <!-- main footer begin -->
+    <div class="main-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="compact-widget">
+                        <div class="widget-inner">
+                            <h3 class="widget-title"><?php _e('Résumé', 'wp-theme-base-translate'); ?></h3>
+                            <p><?php dynamic_sidebar( 'bloc_footer_1' ); ?></p>
+                            <div class="social-icons clearfix">
+                                <a href="<?php the_field('rs_facebook', 'option'); ?>" class="facebook tipped" data-title="facebook"  data-tipper-options='{"direction":"top","follow":"true"}'><i class="fa fa-facebook"></i></a>
+                                <a href="<?php the_field('rs_twitter', 'option'); ?>" class="twitter tipped" data-title="twitter"  data-tipper-options='{"direction":"top","follow":"true"}'><i class="fa fa-twitter"></i></a>
+                                <a href="<?php the_field('rs_googleplus', 'option'); ?>" class="google tipped" data-title="google +"  data-tipper-options='{"direction":"top","follow":"true"}'><i class="fa fa-google-plus"></i></a>
+                                <a href="<?php the_field('rs_youtube', 'option'); ?>" class="youtube tipped" data-title="youtube"  data-tipper-options='{"direction":"top","follow":"true"}'><i class="fa fa-youtube-play"></i></a>
+                                <a href="<?php the_field('rs_linkedin', 'option'); ?>" class="linkedin tipped" data-title="linkedin"  data-tipper-options='{"direction":"top","follow":"true"}'><i class="fa fa-linkedin"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="compact-widget">
+                        <h3 class="widget-title"><?php _e('Catégories', 'wp-theme-base-translate'); ?></h3>
+                        <div class="widget-inner">
+                            <?php
+                            $args = array(
+                                'menu' => 'menu_bas',
+                                'container' => 'ul',
+                            );
+                            wp_nav_menu($args);
+                            ?>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="compact-widget">
+                        <h3 class="widget-title"><?php _e('Contact', 'wp-theme-base-translate'); ?></h3>
+                        <div class="widget-inner">
+                            <?php the_field('footer_adresse', 'option'); ?>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="compact-widget">
+                        <h3 class="widget-title"><?php _e('Newsletter', 'wp-theme-base-translate'); ?></h3>
+                        <div class="widget-inner">
+                            <div class="newsletter newsletter-widget">
+                                <p><?php _e('Stay informed about our news and events', 'wp-theme-base-translate'); ?></p>
+                                <form action="" method="post">
+                                    <p><input class="newsletter-email" type="email" name="email" placeholder="Your email"><i class="fa fa-envelope-o"></i></p>
+                                    <p><input class="newsletter-submit" type="submit" value="Subscribe"></p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- main footer close -->
+
+    <!-- sub footer begin -->
+    <div class="sub-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <?php _e('Copyright &copy; 2016 Designed by AuThemes. All rights reserved.', 'wp-theme-base-translate'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- sub footer close -->
+
+</footer>
+<!-- footer close -->
+
+</div>
 <a id="to-the-top" ><i class="fa fa-angle-up"></i></a>
 
 <!-- LOAD JS FILES -->
