5837a0cc26baaa892c7d4537603fb20a00ecf8e3
tetrabirk <ludovic.leyens@gmail.com>
2018-01-27 20:43:20 +0100

    single

--- single.php
+++ single.php
@@ -1,90 +1,30 @@
+<?php
+/**
+ * Template Name: Article
+ *
+ */
+get_header();
+?>
 
-<section id="subheader" data-speed="8" data-type="background" class="padding-top-bottom subheader">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Nom de l'article</h1>
-                <ul class="breadcrumbs">
-                    <li><a href="index.html">Home</a></li>
-                    <b>/</b>
-                    <li class="active">Nom de l'article</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</section>
+<?php while (have_posts()) : the_post(); ?>
+
+<?php include_once 'include/single/titre.php' ?>
+
 
 <!-- content begin -->
 <div id="content">
     <div class="container">
         <div class="row">
-            <div class="col-md-9">
-                <div class="blog-single">
-                    <!-- post begin -->
-                    <article>
-                        <div class="post-media">
-                            <img src="images/news/blog-details.jpg" class="img-responsive">
-                        </div>
-                        <div class="post-content">
-                            <div class="post-title">
-                                <h1>Nom de l'article</h1>
-                            </div>
-                            <div class="post-metadata">
-                                        <span class="posted-on">
-                                            <i class="fa fa-clock-o"></i>
-                                            November 30, 2016
-                                        </span>
-                                <span class="byline">
-                                            <i class="fa fa-user"></i>
-                                            <a href="#">AuThemes</a>
-                                        </span>
-                                <span class="cat-links">
-                                            <i class="fa fa-folder-open"></i>
-                                            <a href="#">Markup</a>
-                                        </span>
-                            </div>
-                            <div class="hr"></div>
-                            <div class="post-entry">
-                                <p>Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat id. Vivamus interdum urna at sapien varius elementum. Suspendisse ut mi felis et interdum libero lacinia vel. Aenean elementum odio ut lorem cursus, eu auctor magna pellentesque.  Cras facilisis quam sed rhoncus dapibus. Quisque lorem enim, dictum at magna eu, hendrerit hendrerit arcu. Etiam vulputate ac tortor ac gravida. Proin accumsan placerat rutrum. Praesent ut eros ac nisi ultrices rhoncus et.</p>
-                                <p>Nulla fermentum eros vitae est finibus dapibus. Aliquam porta nulla a elit varius efficitur. In in magna sed turpis venenatis tristique eu eget neque. Duis condimentum libero ornare quam tincidunt interdum. Phasellus porttitor nisi ut lectus pellentesque, ut fringilla leo convallis. </p>
-                                <p>Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat id. Vivamus interdum urna at sapien varius elementum. Suspendisse ut mi felis et interdum libero lacinia vel. Aenean elementum odio ut lorem cursus, eu auctor magna pellentesque.  Cras facilisis quam sed rhoncus dapibus. Quisque lorem enim, dictum at magna eu, hendrerit hendrerit arcu. Etiam vulputate ac tortor ac gravida. Proin accumsan placerat rutrum. Praesent ut eros ac nisi ultrices rhoncus et.</p>
-                                <p>Nulla fermentum eros vitae est finibus dapibus. Aliquam porta nulla a elit varius efficitur. In in magna sed turpis venenatis tristique eu eget neque. Duis condimentum libero ornare quam tincidunt interdum. Phasellus porttitor nisi ut lectus pellentesque, ut fringilla leo convallis. </p>
-                                <p>Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat id. Vivamus interdum urna at sapien varius elementum. Suspendisse ut mi felis et interdum libero lacinia vel. Aenean elementum odio ut lorem cursus, eu auctor magna pellentesque.  Cras facilisis quam sed rhoncus dapibus. Quisque lorem enim, dictum at magna eu, hendrerit hendrerit arcu. Etiam vulputate ac tortor ac gravida. Proin accumsan placerat rutrum. Praesent ut eros ac nisi ultrices rhoncus et.</p>
-                                <p>Nulla fermentum eros vitae est finibus dapibus. Aliquam porta nulla a elit varius efficitur. In in magna sed turpis venenatis tristique eu eget neque. Duis condimentum libero ornare quam tincidunt interdum. Phasellus porttitor nisi ut lectus pellentesque, ut fringilla leo convallis. </p>
-                            </div>
-                            <div class="clearfix"></div>
-                        </div>
-                    </article>
-                    <!-- post close -->
-                </div>
+      <?php include_once 'include/single/article.php' ?>
+      <?php include_once 'include/single/widget.php' ?>
 
-
-            </div>
-            <div class="col-md-3">
-                <div class="main-sidebar">
-                    <aside class="widget widget_text">
-                        <h3 class="widget-title">About</h3>
-                        <div class="tiny-border"></div>
-                        <div class="textwidget">
-                            <p>
-                                Nulla eleifend, sapien eget porttitor maximus, nisl ante convallis dolor, nec consequat felis ex a ex. Etiam vestibulum enim euismod dui vestibulum, vitae fringilla nibh consectetur. Integer at volutpat augue. Donec consectetur, est nec laoreet scelerisque, lacus nulla fermentum odio, ut accumsan enim ipsum a justo.
-                            </p>
-                        </div>
-                    </aside>
-                    <aside class="widget widget_categories">
-                        <h3 class="widget-title">Categories</h3>
-                        <div class="tiny-border"></div>
-                        <ul>
-                            <li class="cat-item"><a href="#">Finance</a></li>
-                            <li class="cat-item"><a href="#">Bilan</a></li>
-                            <li class="cat-item"><a href="#">Conseils</a></li>
-                            <li class="cat-item"><a href="#">Juridique</a></li>
-                        </ul>
-                    </aside>
-                </div>
-            </div>
         </div>
     </div>
 </div>
-<!-- content close -->
-       
+
+<?php endwhile; ?>
+
+<!-- content close-->
+<?php
+get_footer();
+?>
