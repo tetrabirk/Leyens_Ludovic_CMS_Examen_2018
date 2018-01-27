8f9c4832ae01b7dee55a9ea58419b0a777349411
tetrabirk <ludovic.leyens@gmail.com>
2018-01-27 15:50:31 +0100

    creation footer et header + debut front-page

--- footer.php
+++ footer.php
@@ -1,26 +1,26 @@
 <a id="to-the-top" ><i class="fa fa-angle-up"></i></a>
 
 <!-- LOAD JS FILES -->
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/imagesloaded.pkgd.min.js"></script>
-<script src="js/easing.js"></script>
-<script src="js/owl.carousel.js"></script>
-<script src="js/jquery.fitvids.js"></script>
-<script src="js/wow.min.js"></script>
-<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/jquery.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/imagesloaded.pkgd.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/easing.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/owl.carousel.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/jquery.fitvids.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/wow.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/jquery.magnific-popup.min.js"></script>
 
 <!-- Waypoints-->
-<script src="js/jquery.waypoints.min.js"></script>
-<script src="js/sticky.min.js"></script>
-<script src="js/tipper.js"></script>
-<script src="js/compact.js"></script>
-<script src="js/custom-index1.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/jquery.waypoints.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/sticky.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/tipper.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/compact.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/custom-index1.js"></script>
 
 <!-- SLIDER REVOLUTION SCRIPTS  -->
-<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
-<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
-<script src="js/revslider-custom.js"></script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri() ?>/js/revslider-custom.js"></script>
 
 <?php wp_footer();?>
 
--- header.php
+++ header.php
@@ -1,7 +1,129 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Ludovic
- * Date: 27/01/2018
- * Time: 15:10
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Compact - Multipurpose Corporate Business Website Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Favicons
+    ================================================== -->
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicon.png" type="image/x-icon">
+
+    <!-- LOAD CSS FILES -->
+    <link href="<?php echo get_stylesheet_directory_uri() ?>/compact_style.css" rel="stylesheet" type="text/css">
+
+    <!-- color scheme -->
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/switcher/demo.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/switcher/colors/blue.css" type="text/css" id="colors">
+
+    <?php wp_head(); ?>
+</head>
+
+<body>
+<!-- Preload images start //-->
+<div class="images-preloader" id="images-preloader">
+    <div class="spinner">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    </div>
+</div>
+<!-- Preload images end //-->
+<div id="wrapper">
+
+    <!-- header begin -->
+    <header class="site-header-1 site-header">
+        <!-- Main bar start -->
+        <div id="sticked-menu" class="main-bar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <!-- logo begin -->
+                        <div id="logo" class="pull-left">
+                            <a href="index.html">
+                                <img src="<?php echo get_stylesheet_directory_uri() ?>/images/logo.png" alt="" class="logo">
+                            </a>
+                        </div>
+                        <!-- logo close -->
+
+                        <!-- btn-mobile menu begin -->
+                        <a id="show-mobile-menu" class="btn-mobile-menu hidden-lg hidden-md"><i class="fa fa-bars"></i></a>
+                        <!-- btn-mobile menu close -->
+
+                        <!-- mobile menu begin -->
+                        <nav id="mobile-menu" class="site-mobile-menu hidden-lg hidden-md">
+                            <ul></ul>
+                        </nav>
+                        <!-- mobile menu close -->
+
+                        <!-- desktop menu begin -->
+                        <nav id="desktop-menu" class="site-desktop-menu hidden-xs hidden-sm">
+                            <ul class="clearfix">
+                                <li class="active">
+                                    <a href="accueil.html">Accueil</a>
+                                </li>
+                                <li>
+                                    <a href="page.html">A propos</a>
+                                </li>
+                                <li>
+                                    <a href="categories.html">Blog</a>
+                                    <ul>
+                                        <li><a href="categories.html">Finance</a></li>
+                                        <li><a href="categories.html">Bilan</a></li>
+                                        <li><a href="categories.html">Conseils</a></li>
+                                        <li><a href="categories.html">Juridique</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <!-- desktop menu close -->
+
+                        <!-- Header Group Button Right begin -->
+                        <div class="header-buttons pull-right hidden-xs hidden-sm">
+
+                            <div class="header-contact">
+                                <ul class="clearfix">
+                                    <li class="phone"><i class="fa fa-phone"></i> <span>0112-826-2789</span></li>
+                                    <li class="border-line">|</li>
+                                </ul>
+                            </div>
+
+                            <!-- Button Modal popup searchbox -->
+                            <div class="search-button">
+                                <!-- Trigger the modal with a button -->
+                                <a href="" data-toggle="modal" data-target="#myModal"><i class="fa fa-search"></i></a>
+                            </div>
+                        </div>
+                        <!-- Header Group Button Right close -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header close -->
+    <div class="gray-line"></div>
+
+    <!-- Modal Search begin -->
+    <div id="myModal" class="modal fade" role="dialog">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <div class="modal-dialog myModal-search">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-body">
+                    <form role="search" method="get" class="search-form" action="">
+                        <input type="search" class="search-field" placeholder="Search here..." value="" title=""/>
+                        <button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal Search close -->
+
