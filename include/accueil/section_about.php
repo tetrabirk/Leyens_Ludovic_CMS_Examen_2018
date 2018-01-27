fa5e44e71c07c326f49be6d91a5e4200d020d243
tetrabirk <ludovic.leyens@gmail.com>
2018-01-27 16:13:22 +0100

    front - section 1

--- include/accueil/section_about.php
+++ include/accueil/section_about.php
@@ -1,7 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Ludovic
- * Date: 27/01/2018
- * Time: 16:05
- */
+<!-- section begin -->
+<section id="section-about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4">
+                <img src="<?php the_field('accueil_1er_section_image',121); ?>" class="img-responsive"/>
+            </div>
+            <div class="col-md-8 col-sm-8">
+                <div class="about-box">
+                    <h2 class="box-title"><?php the_field('accueil_1er_section_texte',121); ?></h2>
+                    <div class="tiny-border"></div>
+                    <p><?php the_field('accueil_1er_section_wysiwyg',121); ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- section close -->
