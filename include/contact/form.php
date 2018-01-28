0b467d248187863400d8c0296c3f98b43a2a6f06
tetrabirk <ludovic.leyens@gmail.com>
2018-01-28 14:51:43 +0100

    contact - titre

---
+++ include/contact/form.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ludovic
+ * Date: 28/01/2018
+ * Time: 12:24
+ */
---
+++ include/contact/titre.php
@@ -0,0 +1,4 @@
+<div class="intro-text text-center">
+    <h2><?php the_title()?></h2>
+    <p><?php the_content()?></p>
+</div>
