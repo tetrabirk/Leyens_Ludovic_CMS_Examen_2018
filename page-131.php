edd972d4e938ba6da54d1fd1ace60ebe5a35a770
tetrabirk <ludovic.leyens@gmail.com>
2018-01-28 12:23:42 +0100

    contact raw

--- page-131.php
+++ page-131.php
@@ -1,7 +1,44 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Ludovic
- * Date: 27/01/2018
- * Time: 15:14
- */
+<!-- content begin -->
+<div id="content" class="no-padding">
+
+    <!-- section begin -->
+    <section id="section-contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="intro-text text-center">
+                        <h2>Contact Us</h2>
+                        <p>Send us an email vulputate bibendum justo sed, tincidunt quisque <br> dictum eget dolor vel maximus.</p>
+                    </div>
+                    <form action="" class="wpcf7-form">
+                        <div class="col-one-third">
+                            <input type="text" placeholder="Your Name">
+                        </div>
+                        <div class="col-one-third margin-one-third">
+                            <input type="email" placeholder="Your Email">
+                        </div>
+                        <div class="col-one-third">
+                            <input type="text" placeholder="Your Phone">
+                        </div>
+                        <div class="col-full"><textarea placeholder="Your Message"></textarea></div>
+                        <div class="clearfix"></div>
+                        <div class="text-center">
+                            <div class="divider-single"></div>
+                            <button class="btn btn-primary btn-big">Send Email</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- section close -->
+
+    <!-- section gmap begin -->
+    <section id="section-gmap" class="no-padding">
+        <div id="map-canvas" class="map-canvas"></div>
+    </section>
+    <!-- section gmap close -->
+
+</div>
+<!-- content close -->
+   
